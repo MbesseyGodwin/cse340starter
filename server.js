dc303a7dae6ef7b233f4d3b6037d402be394c7a9
mbesseygodwin <mbesseygodwin@gmail.com>
2024-01-28 15:41:19 +0100

    added error handling

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  *******************************************/
 
 /* ***********************
- * Require Statements
+ * Require Statements here
  *************************/
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute");
@@ -13,6 +13,8 @@ const express = require("express");
 const env = require("dotenv").config();
 const expressLayouts = require("express-ejs-layouts");
 const path = require('path');  // Added line
+
+const utilities = require('./utilities/index');
 
 const app = express();
 
@@ -23,10 +25,23 @@ app.use(require("./routes/static"));
 // Serve static files from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// index route
+// Index route
+app.get("/", async (req, res, next) => {
+  try {
+    let nav = await utilities.getNav();
+    baseController.buildHome(req, res, { nav });  // Pass the nav variable as part of an object
+  } catch (err) {
+    next(err);
+  }
+});
 
-app.get("/",baseController.buildHome);
+
 app.use("/inv", inventoryRoute);
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
 
 /* ***********************
  * View Engine and Templates
@@ -34,6 +49,22 @@ app.use("/inv", inventoryRoute);
 app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout"); // not at views root
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
 
 /* ***********************
  * Local Server Information
